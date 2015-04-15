1737d6c5a4eaf2349db937dd82a836ea33248670
root <root@lvicpuppetd02.ingramcontent.com>
2015-04-14 20:36:35 -0500

    added oracle linux support and variable binpath, the mesosphere rpms use /usr/bin vs /usr/local/bin

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,13 +7,14 @@ class marathon::params {
   $package_ensure    = 'latest'
   $install_java      = true
   $java_version      = 'java-1.7.0-openjdk'
+  $bin_path          = '/usr/local/bin'
 
   $init_style = $::operatingsystem ? {
     'Ubuntu'             => $::lsbdistrelease ? {
       /(10|12|14)\.04/ => 'upstart',
       default => undef
     },
-    /CentOS|RedHat/      => $::operatingsystemmajrelease ? {
+    /CentOS|RedHat|OracleLinux/      => $::operatingsystemmajrelease ? {
       /(4|5|6)/ => 'sysv',
       default   => 'systemd',
     },
